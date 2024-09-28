f80e69203e986d48aa903e75ccd39c6ddd886b9a
mkb2001 <kevinmugarura051@gmail.com>
2024-09-28 18:32:32 +0300

    Removed unsed variables

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,30 @@
- importScripts(
-   "https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js"
- );
- importScripts(
-  "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js"
- );
+//  importScripts(
+//    "https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js"
+//  );
+//  importScripts(
+//   "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js"
+//  );
 
- const firebaseConfig = {
-  apiKey: "AIzaSyAPZXE1lzpvQHXcvHF0EPp5KQOC4xOWdFo",
-  authDomain: "hello-world-c12cf.firebaseapp.com",
-  projectId: "hello-world-c12cf",
-  storageBucket: "hello-world-c12cf.appspot.com",
-  messagingSenderId: "926761744712",
-  appId: "1:926761744712:web:563524a5403b9d185a9b7d",
-  measurementId: "G-BMR9QQ4NMV"
- };
+//  const firebaseConfig = {
+//   apiKey: "AIzaSyAPZXE1lzpvQHXcvHF0EPp5KQOC4xOWdFo",
+//   authDomain: "hello-world-c12cf.firebaseapp.com",
+//   projectId: "hello-world-c12cf",
+//   storageBucket: "hello-world-c12cf.appspot.com",
+//   messagingSenderId: "926761744712",
+//   appId: "1:926761744712:web:563524a5403b9d185a9b7d",
+//   measurementId: "G-BMR9QQ4NMV"
+//  };
 
- firebase.initializeApp(firebaseConfig);
+//  firebase.initializeApp(firebaseConfig);
 
- const messaging = firebase.messaging();
+//  const messaging = firebase.messaging();
 
- messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.image,
-   };
+//  messaging.onBackgroundMessage((payload) => {
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: payload.notification.image,
+//    };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
- });
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+//  });
